5d811fb83920c297b1b183708cf5fee116d525cc
christhepirate <52164377+christhepirate@users.noreply.github.com>
2019-07-15 11:04:14 -0700

    Add files via upload

---
+++ hello-world.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+echo "Hello World" #prints hello world
+pwd > pwd.txt
+#puts the path of the current directory the user is in when this program is called and puts it into a text file in the same directory
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ task.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+date #prints the current day of the week, date (day, month, year), time (hours, minutes, seconds, AM/PM, timezone) 
+cal #prints a nice little calander
+pwd #prints path of current directory
+ls #prints all files in current directory
